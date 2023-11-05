4ae01dc2acf530ff545aadaa49ff7c65224a0710
Edwin Alfais <2141720193@student.polinema.ac.id>
2023-11-05 08:41:19 +0700

    first commit

---
+++ android/app/src/main/kotlin/com/example/flutter_linkaja_uts_edwin/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutter_linkaja_uts_edwin
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,79 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+  static const tabScreens = [
+  ];
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  int _tabIndex = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'UTS',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(
+            seedColor: const Color.fromARGB(255, 193, 27, 15)),
+        useMaterial3: true,
+      ),
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: MyApp.tabScreens[_tabIndex],
+        floatingActionButton: FloatingActionButton(
+          backgroundColor: const Color.fromARGB(255, 193, 27, 15),
+          onPressed: () {},
+          child: const Icon(
+            Icons.qr_code,
+            color: Colors.white,
+            size: 32,
+          ),
+        ),
+        floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+        bottomNavigationBar: BottomNavigationBar(
+          type: BottomNavigationBarType.fixed,
+          currentIndex: _tabIndex,
+          selectedItemColor: const Color.fromARGB(255, 193, 27, 15),
+          unselectedItemColor: Colors.black,
+          showUnselectedLabels: true,
+          items: const [
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home),
+              label: 'Home',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.history),
+              label: 'History',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(
+                Icons.analytics,
+              ),
+              label: 'Pay',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.mail),
+              label: 'Inbox',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.person),
+              label: 'Profile',
+            ),
+          ],
+          onTap: (index) {
+            if (index > 1) return;
+            setState(() => _tabIndex = index);
+          },
+        ),
+      ),
+    );
+  }
+}
